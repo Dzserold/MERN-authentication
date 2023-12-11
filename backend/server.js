eafc0778ad7d188716014adfb3d4bd1adb02affa
F_Dzserold <f.dzserold@gmail.com>
2023-12-11 22:41:09 +0100

    commit previous, final version

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,16 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
-app.get("/", (req, res) => res.send("server is ready"));
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+  app.get("*", () =>
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => res.send("server is ready"));
+}
 
 app.use(notFound);
 app.use(errorHandler);
